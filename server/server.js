02fe85557099b73c38e6deb907a2567652ad0cc3
Khushi Singh <khushi.singh092001@gmail.com>
2024-07-19 00:07:15 +0530

    cors changes

--- server/server.js
+++ server/server.js
@@ -17,6 +17,13 @@ const bookingSearchRoute = require('./routes/bookingSearchRoute');
 const app = express();
 
 // Middleware
+app.use(function (req, res, next) {
+  //Enabling CORS
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Methods", "GET,HEAD,OPTIONS,POST,PUT");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, x-client-key, x-client-token, x-client-secret, Authorization");
+    next();
+  });
 console.log("url",process.env.FRONTEND_URL)
 app.use(cors({
   origin: [process.env.FRONTEND_URL],
